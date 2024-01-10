296316822592ca1f42deec547b512f98eaa21bca
Psolite <info@bulldevelopers.com>
2024-01-10 05:30:44 -0700

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+This will help you handle your token metadata on Solana
+
+run npm install --save
+change the details in main.ts
+    -<myKeypair>
+    -<mint_address>
+    - update the metadata section with your details
+
+Remember to user the correct image url 
+
+then run ts-node ./main.ts
+
+if you need help doing this reach out on twitter (X) @0xpsoliteSol
---
+++ main.ts
@@ -0,0 +1,71 @@
+
+import {Collection, CreateMetadataAccountV3InstructionAccounts, CreateMetadataAccountV3InstructionDataArgs, Creator, MPL_TOKEN_METADATA_PROGRAM_ID, UpdateMetadataAccountV2InstructionAccounts, UpdateMetadataAccountV2InstructionData, Uses, createMetadataAccountV3, updateMetadataAccountV2, findMetadataPda} from "@metaplex-foundation/mpl-token-metadata";
+import * as web3 from "@solana/web3.js";
+import { PublicKey, createSignerFromKeypair, none, signerIdentity, some } from "@metaplex-foundation/umi";
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { fromWeb3JsKeypair, fromWeb3JsPublicKey} from '@metaplex-foundation/umi-web3js-adapters';
+
+export function loadWalletKey(keypairFile:string): web3.Keypair {
+    const fs = require("fs");
+    const loaded = web3.Keypair.fromSecretKey(
+      new Uint8Array(JSON.parse(fs.readFileSync(keypairFile).toString())),
+    );
+    return loaded;
+  }
+
+const INITIALIZE = false;
+
+async function main(){
+    console.log("let's name some tokens in 2024!");
+    const myKeypair = loadWalletKey("<myKeypair>");
+    const mint = new web3.PublicKey("<mint_address>");
+
+    const umi = createUmi("https://solana-mainnet.core.chainstack.com/0997f7d89de06fbaff45025e88d32b6e");
+    const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair))
+    umi.use(signerIdentity(signer, true))
+
+    // Update your metadata here
+    const ourMetadata = { // TODO change those values!
+        name: "JestJot", 
+        symbol: "JJOT",
+        uri: "https://raw.githubusercontent.com/psolite/testingsolana/master/metadata.json",
+    }
+    const onChainData = {
+        ...ourMetadata,
+        // we don't need that
+        sellerFeeBasisPoints: 0,
+        creators: none<Creator[]>(),
+        collection: none<Collection>(),
+        uses: none<Uses>(),
+    }
+    if(INITIALIZE){
+        const accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint: fromWeb3JsPublicKey(mint),
+            mintAuthority: signer,
+        }
+        const data: CreateMetadataAccountV3InstructionDataArgs = {
+            isMutable: true,
+            collectionDetails: null,
+            data: onChainData
+        }
+        const txid = await createMetadataAccountV3(umi, {...accounts, ...data}).sendAndConfirm(umi);
+        console.log(txid)
+    } else {
+        const data: UpdateMetadataAccountV2InstructionData = {
+            data: some(onChainData),
+            discriminator: 0,
+            isMutable: some(true),
+            newUpdateAuthority: none<PublicKey>(),
+            primarySaleHappened: none<boolean>()
+        }
+        const accounts: UpdateMetadataAccountV2InstructionAccounts = {
+            metadata: findMetadataPda(umi,{mint: fromWeb3JsPublicKey(mint)}),
+            updateAuthority: signer
+        }
+        const txid = await updateMetadataAccountV2(umi, {...accounts, ...data} ).sendAndConfirm(umi);
+        console.log(txid)
+    }
+
+}
+
+main();
